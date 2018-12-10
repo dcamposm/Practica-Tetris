8e14052654c9015f7b6a65a0f47f49ef4a2ff1a9
dcamposm <43445693+dcamposm@users.noreply.github.com>
2018-12-10 21:19:46 +0100

    Tasca 2 acabada

--- Tetris/public_html/codi.js
+++ Tetris/public_html/codi.js
@@ -9,7 +9,7 @@ var Joc = {
     punts: 0,
     puntsMax: 0,
     nivell: 1,
-    pecaActual: null,
+    pecaActual: 0,
     pecaSeguent: null,
     cont: {"i": 0, "j": 0, "l": 0, "o": 0, "s": 0, "z": 0, "t": 0},
     inter: 1000,
@@ -23,7 +23,8 @@ var Joc = {
         Joc.inter= 1000;
         Joc.contInt=0;
         Joc.nextLvl=5;
-                
+        var newPeca=Joc.nextPeca();
+        Joc.pecaActual=new Peca(newPeca[0], newPeca[1],1,2);        
         
         for (var i = 0; i < Joc.espai.length; i++) {
             Joc.espai[i] = new Array(10);
@@ -56,20 +57,20 @@ var Joc = {
         return peces[num];
     },
     
-    /*keyUser: function(e){
+    keyUser: function(e){
         if (e.key==="ArrowUp") {
-            girarPeca;
+            Joc.pecaActual=Joc.pecaActual.rotarHorari();
         }
         else if (e.key==="ArrowDown") {
-            aumentaVel;
+            Joc.pecaActual=Joc.pecaActual.moureAvall();
         }
         else if (e.key==="ArrowRight") {
-            mov=r;
+            Joc.pecaActual=Joc.pecaActual.moureDreta();
         }
         else if (e.key==="ArrowLeft") {
-            mov=l;
+            Joc.pecaActual=Joc.pecaActual.moureEsquerra();
         }
-    },*/
+    },
     
     autoMov: function(){
         this.inici();
@@ -81,6 +82,10 @@ var Joc = {
         clearInterval(game);
     },
     
+    continue: function(){
+        game=setInterval(Joc.mostrar, Joc.inter);
+    },
+    
     mostrar: function(){     
         Joc.punts++;
         Joc.contInt++;
@@ -89,7 +94,7 @@ var Joc = {
             Joc.nivell++;
             Joc.punts+=20;
             Joc.nextLvl+=5;
-            Joc.inter-=10;
+            //Joc.inter-=10;
         }
         else if(Joc.contInt>=Joc.nextLvl) {
             Joc.nivell++;
@@ -99,6 +104,14 @@ var Joc = {
         
         //if (enterPeca=true){this.punts+=10};
         
+        for (var i = 3; i>=0;i--){
+            for (var e = 0; e<4;e++){
+                if (Joc.pecaActual.forma[i][e]===1){
+                    Joc.espai[Joc.pecaActual.posX+i][Joc.pecaActual.posY+e] = "x";  
+                }
+            }
+        }
+        
         var veure="";
         for (var i=0; i<Joc.espai.length; i++){
             veure+="<br>";
@@ -106,6 +119,30 @@ var Joc = {
                 veure+=Joc.espai[i][e]+" ";
             }
         }
+        
+        
+        
+        /*if (Joc.espai[Joc.pecaActual.posX+4][Joc.pecaActual.posY]===0){
+            for (var i = 0; i<4;i++){
+                for (var e = 0; e<4;e++){
+                    Joc.espai[Joc.pecaActual.posX+i][Joc.pecaActual.posY+e] = 0;  
+                }
+            }
+            Joc.pecaActual.posX++;
+        }
+        else if(Joc.espai[Joc.pecaActual.posX+4][Joc.pecaActual.posY]===1) {
+            for (var i = 0; i<4;i++){
+                for (var e = 0; e<4;e++){
+                    if (Joc.pecaActual.forma[i][e]===1){
+                        Joc.espai[Joc.pecaActual.posX+i][Joc.pecaActual.posY+e] = 1;  
+                    }
+                }
+            }
+            var newPeca=Joc.nextPeca();
+            Joc.pecaActual=new Peca(newPeca[0], newPeca[1],1,2); 
+        }*/
+        
+        //Joc.pecaActual;
         
         logPunts="<b>PuntsMax:</b> "+Joc.puntsMax+" <b>PuntsActuals: </b>"+Joc.punts+" <b>Nivell: </b>"+Joc.nivell;
         
@@ -117,14 +154,50 @@ var Joc = {
     
 };
 
-var peca = function(forma, color, posX, posY){
+var Peca = function(forma, color, posX, posY){
     this.forma=forma;
     this.color=color;
     this.posX=posX;
     this.posY=posY;
     
     this.rotarHorari = function(){
-        
+        var formaNova = new Array();
+            for (var i=0;i<this.forma.length;i++) {
+                formaNova[i]=new Array();
+                for (var j=0;j<this.forma[i].length;j++) {
+                    formaNova[i][j]=this.forma[this.forma[i].length-1-j][i];
+                }
+            }
+        this.forma = formaNova;
     };
     
+    this.moureDreta = function(){ 
+        if ((x-1)>0) { 
+            x--;
+            return true;
+        }
+        else { 
+            return false; 
+        }
+    };
+    
+    this.moureEsquerra = function(){
+        if ((x+1)<14) { 
+            x++;
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    
+    this.moureAvall = function(){
+        if ((y+1)<14) { 
+            y++;
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
 };
